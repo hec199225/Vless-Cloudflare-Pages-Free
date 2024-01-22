94ead3cfbe350adc31ea8955aa95cbe05adf0d91
hec199225 <157390138+hec199225@users.noreply.github.com>
2024-01-22 19:09:36 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '1d509442-87fc-46d6-8252-db5eaa8d9ca3';
+let userID = '8ff66d3e-c140-4f4f-a3a6-fc7009fef593';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
